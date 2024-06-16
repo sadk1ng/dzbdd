5cb607e46cd0d95b0d115b70f3b65a54bc2ec7fe
Danila Leonidov <3317451@gmail.com>
2024-06-16 10:37:20 +0300

    initial commit

---
+++ src/test/java/ru/netology/MoneyTransferTest.java
@@ -0,0 +1,39 @@
+package ru.netology;
+
+import com.codeborne.selenide.Selenide;
+import lombok.val;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MoneyTransferTest {
+    private final int amountInvalid = 11000;
+
+    private DashboardPage shouldOpenDashboardPage() {
+        open("http://localhost:9999");
+        Selenide.clearBrowserCookies();
+        Selenide.clearBrowserLocalStorage();
+        val loginPage = new LoginPage();
+        val authInfo = DataHelper.getAuthInfo();
+        val verificationPage = loginPage.validLogin(authInfo);
+        val verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+        return verificationPage.validVerify(verificationCode);
+    }
+
+    @Test
+    void shouldTransferMoneyFromCard2toCard1() {
+        DashboardPage dashboardPage = shouldOpenDashboardPage();
+        dashboardPage.dashboardPageVisible();
+        int amountValid = 500;
+        int expected1 = dashboardPage.getBalanceCard1() + amountValid;
+        int expected2 = dashboardPage.getBalanceCard2() - amountValid;
+        val moneyTransfer = dashboardPage.card1();
+        moneyTransfer.moneyTransferVisible();
+        moneyTransfer.setTransferAmount(amountValid);
+        moneyTransfer.setFrom(DataHelper.getCardNumber2());
+        moneyTransfer.doTransfer();
+        assertEquals(expected1, dashboardPage.getBalanceCard1());
+        assertEquals(expected2, dashboardPage.getBalanceCard2());
+    }
+}
